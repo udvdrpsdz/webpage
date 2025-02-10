d49cd569a3466dc3e30eaba18900e60a3232a93c
udvdrpsdz <udvdr.sdz@gmail.com>
2025-02-10 17:21:01 +0100

    Updated from Publii

--- netlify/functions/page_view_counter.ts
+++ netlify/functions/page_view_counter.ts
@@ -1,39 +1,46 @@
-import { getStore } from "@netlify/blobs";
 import type { Context } from "@netlify/functions";
 
 export default async (req: Request, context: Context) => {
-  try {
-    const urlParams = new URLSearchParams(req.url.split('?')[1]);
-    const page = urlParams.get('page');
-    const view = getStore("view");
-    const dateTime = new Date();
+  return new Response("Hello, world!")
+}
 
-    let entry = await view.get(page);
 
-    if (!entry) {
-      await view.set(page, 1, {
-        metadata: { last_modified: dateTime, previous_count: 0 },
-      });
-    } else {
-      await view.set(page, parseInt(entry) + 1, {
-        metadata: { last_modified: dateTime, previous_count: parseInt(entry) },
-      });
-    }
+//import { getStore } from "@netlify/blobs";
+//import type { Context } from "@netlify/functions";
 
-    entry = await view.get(page);
+//export default async (req: Request, context: Context) => {
+  //try {
+    //const urlParams = new URLSearchParams(req.url.split('?')[1]);
+    //const page = urlParams.get('page');
+    //const view = getStore("view");
+    //const dateTime = new Date();
 
-    return Response.json(
-      {
-        data: {
-          page_url: page,
-          view_count: parseInt(entry),
-          last_modified: dateTime
-        }
-    });
-  }
-  catch (error) {
-    console.error(error);
-    return Response.json({ error: "Internal Server Error" });
-  }
-};
+    //let entry = await view.get(page);
+
+    //if (!entry) {
+      //await view.set(page, 1, {
+        //metadata: { last_modified: dateTime, previous_count: 0 },
+      //});
+    //} else {
+      //await view.set(page, parseInt(entry) + 1, {
+        //metadata: { last_modified: dateTime, previous_count: parseInt(entry) },
+      //});
+    //}
+
+    //entry = await view.get(page);
+
+    //return Response.json(
+      //{
+        //data: {
+          //page_url: page,
+          //view_count: parseInt(entry),
+          //last_modified: dateTime
+        //}
+    //});
+  //}
+  //catch (error) {
+    //console.error(error);
+    //return Response.json({ error: "Internal Server Error" });
+  //}
+//};
 
